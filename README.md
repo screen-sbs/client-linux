0857980728711b11a23421814eb32ff39122b7bf
Jan-Hendrik Ude <68632137+mxve@users.noreply.github.com>
2021-12-01 05:28:11 +0100

    initial

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # screen.sbs
-## linux cli client
+## Linux CLI Client
+
+#### Requirements
+- curl
+- xclip
+- scrot
+
+#### Installation
+##### Option 1: Install .deb
+- ```apt install ./screen-sbs_version-rev_all.deb```
+- Edit config file ```~/.config/screen.conf```
+- Client will be available as ```screen```
+
+##### Option 2: Manual
+- Install requirements
+  - ```apt install curl xclip scrot``` for debian (based)
+- Clone/download to /opt/screen-sbs
+  - ```git clone git@github.com:screen-sbs/client-linux.git /opt/screen-sbs```
+- make screen.sh executable
+  - ```chmod +x /opt/screen-sbs/screen.sh```
+- make screen.sh globally available
+  - ```ln -s /opt/screen-sbs/screen.sh /usr/local/bin/screen```
+- Edit config file ```~/.config/screen.conf```
+- Client will be available as ```screen```
+
+
+#### Usage
+- Take fullscreen screenshot & upload
+  - ```screen``` or
+  - ```screen full``` or
+  - ```screen fullscreen```
+- Take selection screenshot & upload
+  - ```screen area```
+- Upload clipboard
+  - ```screen text```
+
+
+#### Keybinds
+Create keybinds in your DE for the commands above.
+On xfce you can use ```xfce4-keyboard-settings```
---
+++ screen.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+confPath="~/.config/"
+confFile="screen.conf"
+confPath="${confPath/#\~/$HOME}"
+
+if ! test -f "$confPath$confFile"; then
+	mkdir -p $confPath
+	touch $confPath$confFile
+	echo 'saveLocally=true' > $confPath$confFile
+	echo 'openLink=true' >> $confPath$confFile
+	echo 'savePath="~/Documents/screen.sbs/"' >> $confPath$confFile
+	echo 'uploadUrl="https://upload.screen.sbs/"' >> $confPath$confFile
+	echo 'token=""' >> $confPath$confFile
+fi
+source $confPath$confFile
+
+mkdir -p $confPath
+
+if [ "$token" = "" ] ; then
+    echo "Configure your upload token in $confPath"
+	exit 1
+fi
+
+if [ "$saveLocally" = false ] ; then
+    savePath="/tmp/"
+fi
+
+now=$(date +"%Y-%m-%d_%H-%M-%S-%3N")
+dir="${savePath/#\~/$HOME}"
+mkdir -p $dir
+filePath="$dir/$now"
+
+function area {
+	scrot -s "$filePath.png"
+	upload ".png"
+}
+
+function fullscreen {
+	scrot -q 50 "$filePath.png"
+	upload ".png"
+}
+
+function text {
+	xclip -selection "clipboard" -o > "$filePath.txt"
+	upload ".txt"
+}
+
+# required parameters:
+#   $1 file extension
+#     .txt, .png, (.mp4)
+function upload {
+	response=`curl -sF "file=@${filePath}${1}" "$uploadUrl?token=$token"`
+
+	# assume upload was successful if response body starts with http
+	if [[ $response == http* ]]; then
+		echo $response
+		if [ "$openLink" = true ]; then
+			xdg-open $response
+		fi
+	else
+		echo "error while uploading"
+	fi
+}
+
+if [ "$1" = "area" ]; then
+	area
+elif [ "$1" = "" ] || [ "$1" = "full" ] || [ "$1" = "fullscreen" ]; then
+	fullscreen
+elif [ "$1" = "text" ]; then
+	text
+fi
